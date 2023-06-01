2f6393ea03d959a2af221b59e6412820c4b0ddcc
Shehan Maxwell <shehanbbn@gmail.com>
2023-06-01 21:42:58 +0530

    modified App.js added home.component.jsx

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
-import Directory from './components/directory/directory.components';
+import Home from './routes/home/home.component';
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-      imageUrl: 'https://iili.io/Hr6hHf1.jpg',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-      imageUrl: 'https://iili.io/Hr6XmxV.jpg',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-      imageUrl: 'https://iili.io/Hr6XyiP.jpg',
-    },
-    {
-      id: 4,
-      title: 'Masks',
-      imageUrl: 'https://iili.io/Hr6XDDQ.jpg',
-    },
-    {
-      id: 5,
-      title: 'Gloves',
-      imageUrl: 'https://iili.io/Hr6Nk1S.jpg',
-    },
-  ];
-  return <Directory categories={categories} />;
+  return <Home />;
 };
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,34 @@
+import Directory from '../../components/directory/directory.components';
+
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      imageUrl: 'https://iili.io/Hr6hHf1.jpg',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageUrl: 'https://iili.io/Hr6XmxV.jpg',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      imageUrl: 'https://iili.io/Hr6XyiP.jpg',
+    },
+    {
+      id: 4,
+      title: 'Masks',
+      imageUrl: 'https://iili.io/Hr6XDDQ.jpg',
+    },
+    {
+      id: 5,
+      title: 'Gloves',
+      imageUrl: 'https://iili.io/Hr6Nk1S.jpg',
+    },
+  ];
+  return <Directory categories={categories} />;
+};
+
+export default Home;
