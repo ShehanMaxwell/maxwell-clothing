79c69ade7f2ff0471efd2349ef59173a83ad4c8b
Shehan Maxwell <shehanbbn@gmail.com>
2023-06-05 19:21:19 +0530

    modified navigation.component.jsx and firebase.utils.jsx

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,10 +4,15 @@ import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/CrwnLogo.svg';
 import './navigations.styles.scss';
 import { UserContext } from '../../context/user.context';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-  // console.log(currentUser);
+
+  const signOutHandler = async () => {
+    const res = await signOutUser();
+    console.log(res);
+  };
 
   return (
     <Fragment>
@@ -21,7 +26,9 @@ const Navigation = () => {
             SHOP
           </Link>
           {currentUser ? (
-            <span className='nav-link'>SIGN OUT</span>
+            <span className='nav-link' onClick={signOutHandler}>
+              SIGN OUT
+            </span>
           ) : (
             <Link className='nav-link' to='/auth'>
               SIGN IN
