091316627becea2906d06b0160cc50d5ba849a31
Shehan Maxwell <shehanbbn@gmail.com>
2023-06-12 15:12:34 +0530

    modified categories.context.jsx and shop.component.jsx

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -6,12 +6,12 @@ import ProductCard from '../../components/product-card/product-card.component';
 import './shop.styles.scss';
 
 const Shop = () => {
-  const { products } = useContext(CategoriesContext);
+  const { categoriesMap } = useContext(CategoriesContext);
   return (
     <div className='products-container'>
-      {products.map((product) => (
+      {/* {products.map((product) => (
         <ProductCard key={product.id} product={product} />
-      ))}
+      ))} */}
     </div>
   );
 };
