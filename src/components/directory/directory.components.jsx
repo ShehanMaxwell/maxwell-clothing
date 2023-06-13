db7250bdfb2868490a24da5f23e1646e42c0f0cd
Shehan Maxwell <shehanbbn@gmail.com>
2023-06-13 16:46:42 +0530

    modified 4 files

--- src/components/directory/directory.components.jsx
+++ src/components/directory/directory.components.jsx
@@ -1,6 +1,39 @@
 import DirectoryItem from '../directory-item/directory-item.component';
+import { DirectoryContainer } from './directory.styles.scss';
 
-const Directory = ({ categories }) => {
+const categories = [
+  {
+    id: 1,
+    title: 'Hats',
+    imageUrl: 'https://iili.io/Hr6hHf1.jpg',
+    route: 'shop/hats',
+  },
+  {
+    id: 2,
+    title: 'Jackets',
+    imageUrl: 'https://iili.io/Hr6XmxV.jpg',
+    route: 'shop/jackets',
+  },
+  {
+    id: 3,
+    title: 'Sneakers',
+    imageUrl: 'https://iili.io/Hr6XyiP.jpg',
+    route: 'shop/sneakers',
+  },
+  {
+    id: 4,
+    title: 'Masks',
+    imageUrl: 'https://iili.io/Hr6XDDQ.jpg',
+    route: 'shop/womens',
+  },
+  {
+    id: 5,
+    title: 'Gloves',
+    imageUrl: 'https://iili.io/Hr6Nk1S.jpg',
+    route: 'shop/mens',
+  },
+];
+const Directory = () => {
   return (
     <div className='directory-container'>
       {categories.map((category) => (
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -2,36 +2,10 @@ import { Outlet } from 'react-router-dom';
 import Directory from '../../components/directory/directory.components';
 
 const Home = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-      imageUrl: 'https://iili.io/Hr6hHf1.jpg',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-      imageUrl: 'https://iili.io/Hr6XmxV.jpg',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-      imageUrl: 'https://iili.io/Hr6XyiP.jpg',
-    },
-    {
-      id: 4,
-      title: 'Masks',
-      imageUrl: 'https://iili.io/Hr6XDDQ.jpg',
-    },
-    {
-      id: 5,
-      title: 'Gloves',
-      imageUrl: 'https://iili.io/Hr6Nk1S.jpg',
-    },
-  ];
+ 
   return (
     <div>
-      <Directory categories={categories} />
+      <Directory  />
       <Outlet />
     </div>
   );
