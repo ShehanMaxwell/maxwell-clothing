5b493b3dc46428f6238bd02ab352490eacc3ed05
Shehan Maxwell <shehanbbn@gmail.com>
2023-06-13 14:28:19 +0530

    changed 3 files

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,4 @@
-import { ReactComponent as ShoppingIcon } from '../../assets/115 - shopping-bag.svg';
-import './cart-icon.styles.scss';
+import { ShoppingIcon, CartIconContainer, ItemCount } from './cart-icon.styles';
 
 import { CartContext } from '../../context/cart.context';
 import { useContext } from 'react';
@@ -10,10 +9,10 @@ const CartIcon = () => {
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
-    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
+    <CartIconContainer onClick={toggleIsCartOpen}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>{cartCount}</span>
-    </div>
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
