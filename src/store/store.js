0032b7b96e9a9b00c5a0cbc1fc89967ee2d4a541
Shehan Maxwell <shehanbbn@gmail.com>
2023-06-27 16:04:46 +0530

    modified store.js in store

--- src/store/store.js
+++ src/store/store.js
@@ -8,7 +8,7 @@ const loggerMiddleware = (store) => (next) => (action) => {
   }
 
   console.log('type:', action.type);
-  console.log('payload:', action.payload); //
+  console.log('payload:', action.payload); 
   console.log('currentState:', store.getState());
 
   next(action);
