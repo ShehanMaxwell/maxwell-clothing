bb093779b2df4a9df39d6b2d928ca1b1e05ba2ff
Shehan Maxwell <shehanbbn@gmail.com>
2023-06-30 12:42:52 +0530

    modified 3 files

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,15 @@ import { compose, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { legacy_createStore as createStore } from 'redux';
 
 import { rootReducer } from './root-reducer';
-import { legacy_createStore as createStore } from 'redux';//
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-  Boolean
-);
+const middleWares = [
+  process.env.NODE_ENV === 'development' && logger,
+  thunk,
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
